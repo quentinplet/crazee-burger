83ddb9fe2eb09ae5dca4b60586f6ea71a41393c6
Quentin Plet <quentin.plet@gmail.com>
2023-11-29 22:01:26 +0100

    ♻️ FO1:LOGIN> Create LoginForm

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const LoginPage = () => {
+const LoginForm = () => {
   const [inputValue, setInputValue] = useState("");
 
   const handleChange = (event) => {
@@ -13,22 +13,20 @@ const LoginPage = () => {
     setInputValue("");
   };
   return (
-    <>
+    <form action="submit" onSubmit={handleSubmit}>
       <h1>Bienvenue chez nous ! </h1>
       <br />
       <h2>Connectez-vous</h2>
-      <form action="submit" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Entrez votre prénom..."
-          onChange={handleChange}
-          value={inputValue}
-          required
-        />
-        <button>Accédez à votre espace</button>
-      </form>
-    </>
+      <input
+        type="text"
+        placeholder="Entrez votre prénom..."
+        onChange={handleChange}
+        value={inputValue}
+        required
+      />
+      <button>Accédez à votre espace</button>
+    </form>
   );
 };
 
-export default LoginPage;
+export default LoginForm;
---
+++ src/components/pages/login/LoginPage.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import LoginForm from "./LoginForm";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+};
+
+export default LoginPage;
