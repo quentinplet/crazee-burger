815867bd4752ca168e0048c7f2663ee22a6db527
Quentin Plet <quentin.plet@gmail.com>
2023-12-02 20:30:34 +0100

    💄 F03:LOGO>add styling

--- src/components/reusable-ui/Logo.jsx
+++ src/components/reusable-ui/Logo.jsx
@@ -1,12 +1,45 @@
 import styled from "styled-components";
 
 const Logo = () => {
-  return <LogoStyled>Logo CRAZEE BURGER</LogoStyled>;
+  return (
+    <LogoStyled>
+      <h1>CRAZEE</h1>
+      <img src="src/assets/logo-orange.png" alt="logo" />
+      <h1>BURGER</h1>
+    </LogoStyled>
+  );
 };
 
 export default Logo;
 
 const LogoStyled = styled.div`
-  background: blue;
+  border: 1px solid blue;
+  display: flex;
+  align-items: center;
   color: white;
+  margin: 0;
+  padding: 0;
+  /* transform: scale(2.5); */
+
+  h1 {
+    display: inline;
+    text-align: center;
+    color: #ffa01b;
+    font-size: 110px;
+    font-weight: 700;
+    line-height: 1em;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+    font-family: "Amatic SC", cursive;
+    margin: 0;
+    padding: 0;
+  }
+
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 150px;
+    width: 200px;
+    margin: 0 20px;
+  }
 `;
