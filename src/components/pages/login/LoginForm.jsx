487841f726c39446d17b3cd19b3cac1d679305e5
Quentin Plet <quentin.plet@gmail.com>
2023-12-07 15:39:06 +0100

    💄 F03:LOGIN>LoginForm>button>Add styling

--- src/components/pages/login/LoginForm.jsx
+++ src/components/pages/login/LoginForm.jsx
@@ -50,7 +50,7 @@ const LoginForm = () => {
 export default LoginForm;
 
 const LoginFormStyled = styled.form`
-  background: green;
+  /* background: green; */
   text-align: center;
   margin: 0px auto;
   max-width: 500px;
@@ -110,9 +110,9 @@ const LoginFormStyled = styled.form`
       outline: none;
     }
 
-    &&::placeholder {
+    input::placeholder {
       color: #d3d3d3;
-      background: white;
+      background-color: white;
     }
   }
 
@@ -166,51 +166,3 @@ const LoginFormStyled = styled.form`
     }
   }
 `;
-
-/* 
-button {
-  width: 100%;
-  border: 1px solid red;
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  position : relative;
-  white-space: nowrap;
-  text-decoration: none;
-  line height: 1;
-
-  padding: 18px 24 px;
-  border-radius: 5px;
-  font-size: 15px;
-  font-weight: 800;
-  color: white;
-  background-color: #f56a2c;
-  border: 1px solid #f56a2c;
-
-  &:hover:not(:disabled) { 
-    background-color: white;
-    color: #ff9f1b;
-    border: 1px solid #ff9f1b;
-    transition: all 0.2s ease-in-out; 
-  }   
-
-  &:active{
-    color: white;
-    background-color: #f56a2c;
-    border: 1px solid #f56a2c;
-  }
-
-  &:disabled{
-    opacity: 0.6;
-    cursor: not-allowed;
-  }
-
-  .icon{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 15px;
-    margin-left: 10px;
-  }
-
-*/
