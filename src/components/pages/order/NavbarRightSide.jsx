c0bb1ac1b8682d58fc5f63ab37b346b52bb16f1d
Quentin Plet <quentin.plet@gmail.com>
2023-12-08 22:29:36 +0100

    F04:ORDER>Navbar>NavbarRightSide>Profile> add Profile style

--- src/components/pages/order/NavbarRightSide.jsx
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import styled from "styled-components";
-import { BsPersonCircle } from "react-icons/bs";
+import Profile from "./Profile";
 
 const NavbarRightSide = ({ userName }) => {
   return (
     <NavbarRightSideStyled>
-      <div>
-        <h1>
-          Hey, <span>{userName}</span>{" "}
-        </h1>
-        <Link to="/">
-          <button>Se déconnecter</button>
-        </Link>
-      </div>
-      <BsPersonCircle className="icon" />
+      {/* <div className="admin-button">Admin Button</div> */}
+      <Profile userName={userName} />
     </NavbarRightSideStyled>
   );
 };
@@ -26,33 +18,10 @@ const NavbarRightSideStyled = styled.div`
   font-family: "Open Sans", sans-serif;
 
   display: flex;
-  justify-content: center;
   align-items: center;
-  margin-right: 70px;
+  margin-right: 50px;
 
-  h1 {
-    font-size: 16px;
-    font-weight: 400;
-    line-height: 22px;
-    color: #747b91;
-
-    span {
-      color: #ffa01b;
-      font-weight: 700;
-    }
-  }
-
-  button {
-    border: none;
-    color: white;
-    font-size: 10px;
-    font-weight: 400;
-    line-height: 14px;
-    cursor: pointer;
-  }
-
-  .icon {
-    font-size: 36px;
-    color: #747b91;
+  .admin-button {
+    background: lightblue;
   }
 `;
