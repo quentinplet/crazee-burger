54e091a22a310659023c26b629db903c6e63672d
Quentin Plet <quentin.plet@gmail.com>
2024-01-21 15:23:53 +0100

    F09: ADMIN > EditForm > Add Hint Message

--- src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/EditForm.jsx
@@ -1,15 +1,74 @@
-import React from "react";
+import React, { useContext } from "react";
 import styled from "styled-components";
-import EditEmptyMessage from "./EditEmptyMessage";
+import TextInput from "../../../../../reusable-ui/TextInput";
+import ImagePreview from "./ImagePreview";
+import { getInputTextConfig } from "./InputTextConfig";
+import Menu from "../../Menu/Menu";
+import MenuContext from "../../../../../../context/MenuContext";
+import HintMessage from "./HintMessage";
+
+export const EMPTY_PRODUCT = {
+  id: "",
+  title: "",
+  imageSource: "",
+  price: 0,
+};
 
 const EditForm = () => {
+  const { productSelected, setProductSelected, productIsSelected } =
+    useContext(MenuContext);
+
+  const inputTexts = getInputTextConfig(productSelected);
+
+  const handleChange = (event) => {
+    if (productIsSelected) {
+      const { name, value } = event.target;
+      setProductSelected((prevState) => ({
+        ...prevState,
+        [name]: value,
+      }));
+    }
+  };
+
   return (
     <EditFormStyled>
-      <EditEmptyMessage />
+      {/* <HintMessage /> */}
+
+      <ImagePreview imageSource={productSelected.imageSource} />
+      <div className="input-fields">
+        {inputTexts.map((input) => (
+          <TextInput
+            key={input.id}
+            name={input.name}
+            placeholder={input.placeholder}
+            value={productSelected[input.name]}
+            // value={input.value}
+            onChange={handleChange}
+            Icon={input.Icon}
+            version="minimalist"
+          />
+        ))}
+      </div>
     </EditFormStyled>
   );
 };
 
 export default EditForm;
 
-const EditFormStyled = styled.div``;
+const EditFormStyled = styled.div`
+  width: 70%;
+  height: 100%;
+  display: grid;
+  grid-template-columns: 1fr 3fr;
+  grid-template-rows: repeat(4, 1fr);
+  grid-column-gap: 20px;
+  grid-row-gap: 8px;
+
+  .input-fields {
+    grid-area: 1 / 2 / -2 / 3;
+    grid-row-gap: 8px;
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, 1fr);
+  }
+`;
