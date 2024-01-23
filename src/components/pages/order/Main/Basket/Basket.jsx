3e87dd50c794f59546e4a597d204c7d3a009f8b4
Quentin Plet <quentin.plet@gmail.com>
2024-01-23 15:09:15 +0100

    💄 F10:BASKET > Add BasketBody

--- src/components/pages/order/Main/Basket/Basket.jsx
+++ src/components/pages/order/Main/Basket/Basket.jsx
@@ -5,14 +5,13 @@ import Total from "./Total";
 import Header from "../../../../reusable-ui/Header";
 import { formatPrice } from "../../../../../utils/maths";
 import Footer from "./Footer";
+import BasketBody from "./BasketBody";
 
 const Basket = () => {
   return (
     <BasketStyled>
       <Total amountToPay={formatPrice(0.0)} />
-      <div className="body">
-        <p>Votre Commande est vide</p>
-      </div>
+      <BasketBody />
       <Footer />
     </BasketStyled>
   );
@@ -24,28 +23,4 @@ const BasketStyled = styled.div`
   background: pink;
   display: flex;
   flex-direction: column;
-
-  .head {
-    background: red;
-    font-size: 36px;
-    font-weight: 400;
-    display: flex;
-    justify-content: space-between;
-    padding: 12px 16px;
-    height: 70px;
-    background: ${theme.colors.background_dark};
-    color: ${theme.colors.primary};
-  }
-
-  .body {
-    flex: 1;
-    background: ${theme.colors.background_white};
-    box-shadow: ${theme.shadows.basket};
-  }
-
-  .footer {
-    bottom: 0;
-    height: 70px;
-    background: ${theme.colors.background_dark};
-  }
 `;
